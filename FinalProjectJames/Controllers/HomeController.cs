9976192336d035c29d059913cdbf6f4edb17fc1a
Reed James <reedtjames1234@gmail.com>
2021-04-28 10:46:23 -0500

    add controller & dummy data

---
+++ FinalProjectJames/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectJames.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.loanAmount = 15000;
+            ViewBag.interestRate = .03;
+            ViewBag.yearsOfLoan = 15;
+            ViewBag.monthlyPayment = 1000.33;
+            return View();
+        }
+    }
+}

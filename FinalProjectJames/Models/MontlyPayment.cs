5c7f174ab64b2a5fc23102a5138cff39a79520fd
Reed James <reedtjames1234@gmail.com>
2021-04-30 11:16:59 -0500

    add monthpay class attributes, add attribute tests, tests green

---
+++ FinalProjectJames/Models/MontlyPayment.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectJames.Models
+{
+    public class MontlyPayment
+    {
+        //Attributes
+        private double _loanAmount;
+        private decimal _interestRate;
+        private int _yearsOfLoan;
+        private double _montlyPayment;
+        public double LoanAmount
+        {
+            get { return _loanAmount; }
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException("Input for loan amount must be higher than $1");
+                }
+                _loanAmount = value;
+            }
+        }
+        public decimal InterestRate
+        {
+            get { return _interestRate; }
+            set
+            {
+                if (value <= 0 || value >= 1)
+                {
+                    throw new ArgumentOutOfRangeException("Input must be between .01 and .99");
+                }
+                _interestRate = value;
+            }
+        }
+        public int YearsOfLoan
+        {
+            get { return _yearsOfLoan; }
+            set
+            {
+                if(value < 1)
+                {
+                    throw new ArgumentOutOfRangeException("Input must be 1 or greater");
+                }
+                _yearsOfLoan = value;
+            }
+        }
+        public double MonthlyPayment
+        {
+            get { return _montlyPayment; }
+        }
+        //Constructor
+        public MontlyPayment() { }
+        //Methods
+        public decimal GetMontlyInterestRate(decimal iRate)
+        {
+            decimal mRate;
+            mRate = iRate;
+            return mRate;
+        }
+
+
+    }
+}
--- FinalProjectTest/UnitTest1.cs
+++ FinalProjectTest/UnitTest1.cs
@@ -1,16 +1,41 @@
 using System;
 using Xunit;
+using FinalProjectJames;
 
 namespace FinalProjectTest
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestLoanAmountLessThanOne()
         {
-            bool expected = false;
-            bool actual = true;
-            Assert.Equal(expected, actual);
+            FinalProjectJames.Models.MontlyPayment mp = new FinalProjectJames.Models.MontlyPayment();
+            int lessThanOne = 0;
+            Assert.Throws<ArgumentOutOfRangeException>(() => mp.LoanAmount = lessThanOne);
+        }
+
+        [Fact]
+        public void TestInterestRateLessThanZero()
+        {
+            FinalProjectJames.Models.MontlyPayment mp = new FinalProjectJames.Models.MontlyPayment();
+            decimal lessThanOne = -.01M;
+            Assert.Throws<ArgumentOutOfRangeException>(() => mp.InterestRate = lessThanOne);
+        }
+
+        [Fact]
+        public void TestInterestRateGreaterThanNineNine()
+        {
+            FinalProjectJames.Models.MontlyPayment mp = new FinalProjectJames.Models.MontlyPayment();
+            int greaterThanNineNine = 1;
+            Assert.Throws<ArgumentOutOfRangeException>(() => mp.InterestRate = greaterThanNineNine);
+        }
+
+        [Fact]
+        public void TestYearsOfLoanLessThanOne()
+        {
+            FinalProjectJames.Models.MontlyPayment mp = new FinalProjectJames.Models.MontlyPayment();
+            int lessThanOne = 0;
+            Assert.Throws<ArgumentOutOfRangeException>(() => mp.YearsOfLoan = lessThanOne);
         }
     }
 }

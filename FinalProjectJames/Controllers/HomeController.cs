0be35709050182c47c6dedf4340522e4cf872348
Reed James <reedtjames1234@gmail.com>
2021-05-01 21:17:29 -0500

    add homecontroller get & post, project works, inputs and calculates correctly

--- FinalProjectJames/Controllers/HomeController.cs
+++ FinalProjectJames/Controllers/HomeController.cs
@@ -3,18 +3,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using FinalProjectJames.Models;
 namespace FinalProjectJames.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.MonPayment = 0;
+            return View();
+            /*
             ViewBag.loanAmount = 15000;
             ViewBag.interestRate = .03;
             ViewBag.yearsOfLoan = 15;
             ViewBag.monthlyPayment = 1000.33;
             return View();
+            */
+        }
+
+        [HttpPost]
+        public IActionResult Index(FinalProjectJames.Models.MontlyPayment model)
+        {
+            model.GetMontlyPayment();
+            ViewBag.MonPayment = model.MonthlyPayment;
+            return View(model);
         }
     }
 }
